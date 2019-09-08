eba7552369ed891c2647340c5ce133e2cd69296a
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-08 17:48:20 +0200

    Implement form for upload image

--- src/app/components/photo-form/photo-form.component.ts
+++ src/app/components/photo-form/photo-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoFormComponent implements OnInit {
 
+  file: File;
+  photoSelected: string | ArrayBuffer;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onPhotoSelected(event: HtmlInputEvent): void {
+    if (event.target.files && event.target.files[0]) {
+      this.file = event.target.files[0];
+      // Image preview
+      const reader = new FileReader();
+      reader.onload = () => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+  uploadPhoto(title: HTMLInputElement, description: HTMLTextAreaElement): boolean {
+    console.log('Upload photo...');
+    console.log(title);
+    console.log(description);
+    console.log(this.file);
+    return false;
   }
 
 }
